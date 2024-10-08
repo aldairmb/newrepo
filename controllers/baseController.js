54a769198c40e6f74bf17215fee1f25c311362aa
aldairmb <aldairmorales.banda@gmail.com>
2024-10-07 18:17:58 -0600

    lost

---
+++ controllers/baseController.js
@@ -0,0 +1,9 @@
+const utilities = require("newrepo/utilities")
+const baseController = {}
+
+baseController.buildHome = async function(req, res){
+  const nav = await utilities.getNav()
+  res.render("index", {title: "Home", nav})
+}
+
+module.exports = baseController
